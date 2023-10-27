66ad0f70ba89e7f732743788d10bcd928b781fe2
avkolesoff <avkolesoff@gmail.com>
2023-10-27 14:00:25 +0300

    EstimateFile Model and db services

---
+++ App2/Models/EstimateFile.cs
@@ -0,0 +1,12 @@
+﻿using SQLite;
+
+namespace App2.Models
+{
+    class EstimateFile
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string FileName { get; set; }
+
+    }
+}

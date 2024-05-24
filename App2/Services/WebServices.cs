637aec52104b9829acff2176d83d9bb0fd177f30
avkolesoff <avkolesoff@gmail.com>
2024-05-24 05:53:08 +0300

    final commit

--- App2/Services/WebServices.cs
+++ App2/Services/WebServices.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Net.Http;
 using Xamarin.Essentials;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Opera;
 
 namespace App2.Services
 {
@@ -9,33 +11,7 @@ namespace App2.Services
     {
         public static string Parse(string productURL)
         {
-            string price = string.Empty;
-            try
-            {
-                using (var hdl = new HttpClientHandler() { AllowAutoRedirect = false, AutomaticDecompression = System.Net.DecompressionMethods.Deflate | System.Net.DecompressionMethods.GZip })
-                {
-                    using (var clnt = new HttpClient(hdl))
-                    {
-                        using (var resp = clnt.GetAsync(productURL).Result)
-                        {
-                            if (resp.IsSuccessStatusCode)
-                            {
-                                var html = resp.Content.ReadAsStringAsync().Result;
-                                if (!string.IsNullOrEmpty(html))
-                                {
-                                    HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
-                                    doc.LoadHtml(html);
-                                    price = doc.DocumentNode.SelectSingleNode(@"/html/body/div[1]/div/div/div[5]/div/div/div[1]/div/div[2]/div[4]/span[1]").InnerText;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            catch (Exception)
-            {
-            }
-            return price;
+            return "null";
         }
 
 
